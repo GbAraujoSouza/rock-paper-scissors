aa4683c5485321129f9392deda252716d6d0bf76
Gabriel de Araujo de Souza <gabriel.souzaaraujo109@gmail.com>
2022-08-19 22:25:46 -0300

    playRound now returns object with winner, loser and their weapons

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const rockButton = document.querySelector('#rock');
 const scissorsButton = document.querySelector('#scissors');
 const paperButton = document.querySelector('#paper');
 const resultDiv = document.querySelector('#result');
+let playerScore;
+let computerScore;
+
 
 function getComputerChoice() {
-    // criate a random index to access the array of weapons
+    // create a random index to access the array of weapons
     // note that we need integers between 0 and (1 - weaponList.length)
     // floor method clamps the number to the nearest lower integer
     let randomIndex = Math.floor(Math.random() * weaponList.length);
@@ -14,40 +17,80 @@ function getComputerChoice() {
 
 function playRound(playerChoice ,computerChoice) {
     // Check each player possible choice and compare with each possible computer choice
+    let result = {'winnerName': '', 'winnerWeapon': '', 'loserName': '', 'loserWeapon': ''};
     switch (playerChoice) {
         case 'rock':
             switch (computerChoice) {
                 case 'rock':
-                    return 'Tie!';
+                    result['winnerName'] = 'tie';
+                    result['winnerWeapon'] = 'rock';
+                    result['loserName'] = 'tie';
+                    result['loserWeapon'] = 'rock';
+                break;
+
                 case 'paper':
-                    return 'You Lose! Paper beats Rock';
+                    result['winnerName'] = 'computer';
+                    result['winnerWeapon'] = 'paper';
+                    result['loserName'] = 'player';
+                    result['loserWeapon'] = 'rock';
+                break;
+
                 case 'scissors':
-                    return 'You Win! Rock beats Scissors';
+                    result['winnerName'] = 'player';
+                    result['winnerWeapon'] = 'rock';
+                    result['loserName'] = 'computer';
+                    result['loserWeapon'] = 'scissors';
             }
             break;
         case 'paper':
             switch (computerChoice) {
                 case 'rock':
-                    return 'You Win! Paper beats Rock';
+                    result['winnerName'] = 'player';
+                    result['winnerWeapon'] = 'paper';
+                    result['loserName'] = 'computer';
+                    result['loserWeapon'] = 'rock';
+                break;
+                    
                 case 'paper':
-                    return 'Tie!';
+                    result['winnerName'] = 'tie';
+                    result['winnerWeapon'] = 'paper';
+                    result['loserName'] = 'tie';
+                    result['loserWeapon'] = 'paper';
+                break;
+
                 case 'scissors':
-                    return 'You Lose! Scissors beats Paper';
+                    result['winnerName'] = 'computer';
+                    result['winnerWeapon'] = 'scissors';
+                    result['loserName'] = 'player';
+                    result['loserWeapon'] = 'paper';
             }
             break;
         case 'scissors':
             switch (computerChoice) {
                 case 'rock':
-                    return 'You Lose! Rock beats Scissors';
+                    result['winnerName'] = 'computer';
+                    result['winnerWeapon'] = 'rock';
+                    result['loserName'] = 'player';
+                    result['loserWeapon'] = 'scissors';
+                break;
+
                 case 'paper':
-                    return 'You Win! Scissors beats Paper';
+                    result['winnerName'] = 'player';
+                    result['winnerWeapon'] = 'scissors';
+                    result['loserName'] = 'computer';
+                    result['loserWeapon'] = 'paper';
+                break;
+
                 case 'scissors':
-                    return 'Tie!';
+                    result['winnerName'] = 'tie';
+                    result['winnerWeapon'] = 'scissors';
+                    result['loserName'] = 'tie';
+                    result['loserWeapon'] = 'scissors';
             }
             break;
-        default:
-            return 'You shoud chose "Rock", "Paper" or "Scissors"!'
     }
+
+    return result;
 }
 
 function updateScore(winner){
